77b842c77bfd0b81e2a15af09c929c50355479eb
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2021-11-28 20:14:39 -0800

    add voter and candidate actions

--- client/src/components/CandidateActions.jsx
+++ client/src/components/CandidateActions.jsx
@@ -1,3 +1,22 @@
+import { useState } from "react";
+import { Button } from "./Button";
+
 export const CandidateActions = () => {
-  return <div className="CandidateActions">Candidate actions</div>;
+  const [show, setShow] = useState(false);
+
+  return (
+    <div>
+      <div className="ContractActions-title" onClick={() => setShow(!show)}>
+        Candidates
+      </div>
+      {show && (
+        <div className="ContractActions-body">
+          <div className="ContractActions-action">
+            <div className="ContractActions-label">Register your project</div>
+            <Button>Become a candidate</Button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
--- client/src/components/VoterActions.jsx
+++ client/src/components/VoterActions.jsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+import { Button } from "./Button";
+import { TextInput } from "./TextInput";
+
 export const VoterActions = () => {
-  return <div className="VoterActions">Voter actions</div>;
+  const [show, setShow] = useState(false);
+  const buyin = 50000;
+
+  return (
+    <div>
+      <div className="ContractActions-title" onClick={() => setShow(!show)}>
+        Voters
+      </div>
+      {show && (
+        <div className="ContractActions-body">
+          <div className="ContractActions-action">
+            <div className="ContractActions-label">{`Voter registration NFT costs ${buyin}`}</div>
+            <Button>Mint voter NFT</Button>
+          </div>
+          <div className="ContractActions-action">
+            <div className="ContractActions-label">Vote for a project</div>
+            <TextInput placeholder="project address" />
+            <Button>Vote</Button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
